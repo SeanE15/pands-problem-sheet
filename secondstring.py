c50f065efa3c567cd3a1960d3028f23b512f4971
Sean Elliott <G00411288@gmit.ie>
2022-02-06 13:08:00 +0000

    First attempt at secondstring

---
+++ secondstring.py
@@ -0,0 +1,22 @@
+# This is a program that asks the user to input a string 
+# and outputs every second letter in reverse order.
+# Author: Sean Elliott 
+
+question = input('Please enter a sentence: ')
+print(question) 
+
+rawString = input((question)
+reversedString = rawString.reverse
+
+#stringLength = len(input) 
+#slicedString =input[stringlength:: 2] 
+#print (slicedString) 
+
+
+
+#rawString = input("The quick brown fox jumps over the lazy dog."
+#normalisedString = rawStringreverse.strip().lower()
+
+
+#lengthOfRawstring = len(rawString)
+#lengthOfNormalised = len(normalisedString) 
