5b3bf2e6d7ba9796119f7f5a981b41d64a5f4eff
Sean Elliott <G00411288@gmit.ie>
2022-02-11 19:26:37 +0000

    First Attempt at Wk04

---
+++ collatz.py
@@ -0,0 +1,14 @@
+# This is a program that asks the user to input any positive integer
+# and outputs the successive values of the following calculation.
+# At each step calculate the next value by taking the current value and, if it is even, divide it by two,
+# but if it is odd, multiply it by three and add one. The program must end at value of 1.
+# Author: Sean Elliott 
+
+print(input(" Please enter a positive integer: ")) 
+
+counts = 0 
+while counts  0:
+    print(counts)
+    if (counts % 2) == str(0)
+    print(counts) % str(2) 
+    else print(count * 3) + str(1) 
