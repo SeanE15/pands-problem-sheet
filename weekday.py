3501be019028f99ab6a4d142bf694db2a5af0952
Sean Elliott <G00411288@gmit.ie>
2022-02-20 13:38:05 +0000

    First Attempt at weekday.py

---
+++ weekday.py
@@ -0,0 +1,15 @@
+# Write a program that outputs whether or not today is a weekday.
+# Author: Sean Elliott 
+
+days = ("Monday",
+        "Tuesday",
+        "Wednesday", 
+        "Thursday", 
+        "Friday", 
+        "Saturday", 
+        "Sunday"
+            )
+
+weekend = days[5:7]
+for days in weekend: 
+    print(weekend) 
