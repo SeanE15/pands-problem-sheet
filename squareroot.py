38133d411a3bce392b92280d51608543c353eae9
Sean Elliott <G00411288@gmit.ie>
2022-02-26 14:43:23 +0000

    started squareroot.py but struggling

---
+++ squareroot.py
@@ -0,0 +1,16 @@
+# Write a program that takes a positive floating-point number as input and outputs an approximation of its square root.
+# You should create a function called <tt>sqrt</tt> that does this.
+# Author: Sean Elliott 
+
+def newtonMethod:   
+    root = 0.5 * (number + (a/number)) 
+
+
+a = input("Please enter a positive number: ") 
+
+
+
+
+input("Please enter a positive number: ") 
+result = 
+print ("The square root of {} is approximately {}") 
